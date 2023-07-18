d912e3580e4b14ae20f6ad6da7329abf5e8a1ef3
Joy Chowdhury <69787432+Joy097@users.noreply.github.com>
2023-07-18 12:24:46 +0600

    Delete Django Rest 1 directory

--- Django Rest 1/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.getData),
-    path('add/',views.addData),
-    path('rem/<int:pk>',views.remove),
-    path('upd/<int:pk>',views.update)
-]
